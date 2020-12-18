07ba1b150d26c165e446f238b9d35287c8f1dfe3
SumitomShome <sumitomshome@gmail.com>
2020-12-18 18:52:05 +0530

    [Sumitom] Add: Method to use delegates

---
+++ DelegatePracticeProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+delegate int ChangingNumber(int n);
+namespace DelegatePracticeProgram
+{
+        class TestDelegate
+        {
+            static int num = 10;
+            public static int AddNumber(int p)
+            {
+                num += p;
+                return num;
+            }
+            public static int MultiplyNumber(int q)
+            {
+                num *= q;
+                return num;
+            }
+            public static int getNum()
+            {
+                return num;
+            }
+        static void Main(string[] args)
+        {
+            ChangingNumber nc1 = new ChangingNumber(AddNumber);
+            ChangingNumber nc2 = new ChangingNumber(MultiplyNumber);
+            Console.WriteLine("Hello World!");
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+        }
+    }
+}
